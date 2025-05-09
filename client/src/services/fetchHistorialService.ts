6ad4d33291ebfa09737baa2da4bfc7547559491d
Woocky17 <davidortetsabater@gmail.com>
2025-05-09 16:08:42 +0200

    feat: implement chat history fetching and error handling; refactor chat component

--- client/src/services/fetchHistorialService.ts
+++ client/src/services/fetchHistorialService.ts
@@ -1,6 +1,6 @@
 import { MessageChat } from "../types/MessageChat";
 
-export const getHistorial = async (): Promise<MessageChat[]> => {
+export const fetchHistorialService = async (): Promise<MessageChat[]> => {
   try {
     const response = await fetch("http://localhost:4000/api/view_hist");
     if (!response.ok) {
